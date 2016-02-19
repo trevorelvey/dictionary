15466a0b2e1b386c318a6b6a3dc2e5289f94ae04
Trevor <trevor.elvey@gmail.com>
2016-02-19 10:21:49 -0800

    pass instances test for definitions

--- src/main/java/Definition.java
+++ src/main/java/Definition.java
@@ -1,15 +1,23 @@
 import java.util.ArrayList;
 
 public class Definition {
+  private static ArrayList<Definition> instances = new ArrayList<Definition>();
 
   private String mDef;
+  private int mId;
 
   public Definition(String def) {
     mDef = def;
+    instances.add(this);
+    mId = instances.size();
   }
 
   public String getDef() {
     return mDef;
+  }
+
+  public static ArrayList<Definition> all() {
+    return instances;
   }
 
 
--- src/test/java/DefinitionTest.java
+++ src/test/java/DefinitionTest.java
@@ -11,8 +11,17 @@ public class DefinitionTest {
     assertEquals(true, myDefinition instanceof Definition);
   }
 
+  @Test
   public void definition_instantiatesWithDef_true() {
     Definition myDefinition = new Definition("defexample");
     assertEquals("defexample", myDefinition.getDef());
+  }
+
+  @Test
+  public void all_returnsAllInstancesOfDefinition_true() {
+    Definition firstDefinition = new Definition("defexample");
+    Definition secondDefinition = new Definition("defexample2");
+    assertTrue(Definition.all().contains(firstDefinition));
+    assertTrue(Definition.all().contains(secondDefinition));
   }
 }
